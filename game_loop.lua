c1a9057c538737e2bb1fac1076555916ab17cc1e
Steven Johnson <steve@xibalbastudios.com>
2014-11-29 01:14:46 -0600

    Various game loop utilities (mostly extracted from game.LevelMap), for use in boilerplate loop config

--- game_loop.lua
+++ game_loop.lua
@@ -24,13 +24,111 @@
 --
 
 -- Standard library imports --
+local ceil = math.ceil
 local ipairs = ipairs
 
 -- Modules --
+local require_ex = require("tektite_core.require_ex")
+local dots = require("s3_utils.dots")
+local enemies_ok, enemies = pcall(require, "game.Enemies")
+local event_blocks = require("s3_utils.event_blocks")
+local global_events = require("s3_utils.global_events")
+local loop = require_ex.Lazy("corona_boilerplate.game.loop")
 local persistence = require("corona_utils.persistence")
+local player = require("game.Player")
+local tile_maps = require("s3_utils.tile_maps")
+
+-- Corona globals --
+local display = display
+local timer = timer
 
 -- Exports --
 local M = {}
+
+--
+local function NoOp () end
+
+-- Helper to iterate on possibly empty tables
+local function Ipairs (t)
+	if t then
+		return ipairs(t)
+	else
+		return NoOp
+	end
+end
+
+--- DOCME
+function M.AddThings (current_level, level)
+	-- Add the tiles to the level...
+	local tgroup = tile_maps.NewImageGroup()
+
+	current_level.tiles_layer:insert(tgroup)
+
+	tile_maps.AddTiles(tgroup, level)
+
+	-- ...and the event blocks...
+	for _, block in Ipairs(level.event_blocks) do
+		event_blocks.AddBlock(block)
+	end
+
+	-- ...and the dots...
+	for _, dot in Ipairs(level.dots) do
+		dots.AddDot(current_level.things_layer, dot)
+	end
+
+	-- ...and the player...
+	player.AddPlayer(current_level.things_layer, level.start_col, level.start_row)
+
+	-- ...and the enemies...
+	for _, enemy in Ipairs(enemies_ok and level.enemies) do
+		enemies.SpawnEnemy(current_level.things_layer, enemy)
+	end
+
+	-- ...and any global events.
+	global_events.AddEvents(level.global_events)
+end
+
+-- Primary display groups --
+local Groups = { "game_group", "hud_group" }
+
+--- DOCME
+function M.Cleanup (current_level)
+	for _, name in ipairs(Groups) do
+		display.remove(current_level and current_level[name])
+	end
+end
+
+--- DOCME
+function M.BeforeEntering (w, h)
+	return function(view, current_level, level, level_list)
+		-- Record some information to pass along via dispatch.
+		current_level.ncols = level.ncols
+		current_level.nrows = ceil(#level / level.ncols)
+		current_level.w = w
+		current_level.h = h
+
+		-- Add the primary display groups.
+		for _, name in ipairs(Groups) do
+			current_level[name] = display.newGroup()
+
+			view:insert(current_level[name])
+		end
+
+		-- Add game group sublayers, duplicating them in the level info for convenience.
+		for _, name in ipairs{ "bg_layer", "tiles_layer", "decals_layer", "things_layer", "markers_layer" } do
+			local layer = display.newGroup()
+
+			current_level[name] = layer
+
+			current_level.game_group:insert(layer)
+		end
+
+		-- Add the level background, falling back to a decent default if none was given.
+		local bg_func = level.background or level_list.DefaultBackground
+
+		bg_func(current_level.bg_layer, current_level.ncols, current_level.nrows, w, h)
+	end
+end
 
 -- Tile names, expanded from two-character shorthands --
 local Names = {
@@ -49,6 +147,16 @@ function M.DecodeTileLayout (level)
 	for i, tile in ipairs(level.tiles.values) do
 		level[i] = Names[tile] or false
 	end
+end
+
+--- DOCME
+-- On(win): unload the level and do win-related logic
+function M.ExtendWinEvent ()
+	global_events.ExtendAction("win", function()
+		timer.performWithDelay(loop.GetWaitToEndTime(), function()
+			loop.UnloadLevel("won")
+		end)
+	end)
 end
 
 --- DOCME
